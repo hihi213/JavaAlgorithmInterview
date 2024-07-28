0b9464ba5953ded9afcda271eca10fcabbf429fc
hihi213 <149920088+hihi213@users.noreply.github.com>
2024-07-28 18:57:52 +0900

    Update _3Sum15.java

--- src/_3Sum15.java
+++ src/_3Sum15.java
@@ -7,13 +7,15 @@ import java.util.*;
 3.   List<List<Integer>> results = new ArrayList<>();
 제네릭 타입을 사용할 때, 내부의 타입을 명시하지 않아도 컴파일러가 이를 추론합니다.
 따라서 new ArrayList<>()는 new ArrayList<List<Integer>>()와 동일한 결과를 낳습니다.
+4.  리스트나 배열은 참조(reference)로 처리됩니다. 즉, 리스트나 배열을 다른 변수에 할당하거나 메서드에 전달할 때 실제 데이터가 복사되는 것이 아니라 데이터에 대한 참조가 전달됩니다.
+그래서 내가 리스트에 배열[1,2]를 저장후 배열 [2,1]로 직접 바꾸면 리스트의 데이터도 2,1로 바뀜
 */
-//궁금해졌다.
 public class _3Sum15 {
     public static void main(String[] args) {
-        _3Sum15 t=new _3Sum15();
+        _3Sum15 t = new _3Sum15();
         t.threeSum(new int[]{3,0,-2,-1,1,2});
     }
+
     public List<List<Integer>> threeSum(int[] nums) {
         int left = 0, right = 0, zero = 0, minus = 0, nownum = 0,last=0;
         List<List<Integer>> results = new ArrayList<>();
@@ -28,9 +30,7 @@ public class _3Sum15 {
                 if (nownum == 0) {
                     ++zero;
                 } else {
-                    if (!repeat.contains(nownum)) {
-                        repeat.add(nownum);
-                    }
+                    if (!repeat.contains(nownum)){repeat.add(nownum);}
                 }
             }
             else {
@@ -47,6 +47,7 @@ public class _3Sum15 {
         if (!repeat.isEmpty()) {
             for (int i = 0; i < repeat.size(); i++) {
                 if(number.contains(repeat.get(i)*(-2))){
+
                     if (repeat.get(i)<0){
                         results.add(Arrays.asList(repeat.get(i),repeat.get(i),(repeat.get(i)*(-2)))
                         );}
@@ -58,29 +59,35 @@ public class _3Sum15 {
         }
         Collections.sort(number);
         left=number.get(0);
+        int what=left;
         number.remove(0);
-        right=1;
         while (true) {
             if (number.size()<2) {
                 break;
             }
             last=-left-number.get(right);
-            if (number.contains(last)&&number.get(right)!=last&&left!=last) {
 
+            if (number.contains(last)&&what!=right&&right!=0&&last!=number.get(right)) {
+                System.out.println(left+"   "+ number.get(right)+"   "+last+"  "+number+" "+what);
+                Integer[] t= new Integer[3];
+                t[0]=left;
+                t[1]=last;
+                t[2]=number.get(right);
+                Arrays.sort(t);
                 //저장
-                results.add(Arrays.asList(left, number.get(right), (-left -number.get(right))
-                ));
-                //System.out.println(number+"[지운후]"+left+" "+ number.get(right)+" "+last);
+                results.add(Arrays.asList(t));
+                what=right;
+               //System.out.println(number+"[지운후]"+left+" "+ number.get(right)+" "+last);
             }
             right++;
 
             if (right == number.size()) {
                 left=number.get(0);
                 number.remove(0);
-                right = 1;
+                right = 0;
             }
             if (1== number.size()) {
-                // System.out.println(results);
+                //System.out.println(results);
                 break;
             }
         }
